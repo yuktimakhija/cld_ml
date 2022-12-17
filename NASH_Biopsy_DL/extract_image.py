98ea877abc55609a87eb3905778d0250a87331c4
yuktimakhija <bb1190067@iitd.ac.in>
2022-12-17 15:21:45 +0530

    extract biopsy images

--- NASH_Biopsy_DL/extract_image.py
+++ NASH_Biopsy_DL/extract_image.py
@@ -2,6 +2,9 @@ import lxml.etree as ET
 import requests
 import os
 import docx
+import scandir
+import pandas as pd
+from tqdm import tqdm 
 
 def img_from_xml(xml_file_name):
     tree = ET.parse(xml_file_name)
@@ -21,7 +24,7 @@ def img_from_xml(xml_file_name):
         # Save the image to a file
         # To use image_name: os.path.basename(url)
         # To use image number: count
-        open(os.path.join('images', xml_file_name+'_'+count), 'wb').write(response.content)
+        open(os.path.join('biopsy_images', xml_file_name+'_'+count), 'wb').write(response.content)
 
 def img_from_docx(docx_file_name):
     # To use install python-docx
@@ -35,11 +38,23 @@ def img_from_docx(docx_file_name):
             # Get the image file name
             image_file = shape.filename
             # Save the image to a file
-            open(os.path.join('images', image_file), 'wb').write(shape.blob)
+            open(os.path.join('biopsy_images', image_file), 'wb').write(shape.blob)
 
-def main(folder_path):
-    dir_list = os.listdir(folder_path)[:1]
-    for fname in dir_list:
-        patient_id = fname[:-4]
-        # eg: patient_id = 'IPID0073044'
+# def NAS_from_docx(docx_file_name):
+    
+
+def main(folder_path,excel_path):
+    # dir_list = os.listdir(folder_path)[:1]
+
+    df = pd.read_excel(excel_path).set_index('SLIDE NO')
+    for slide_number in tqdm(df.index):
+        if slide_number.endswith('22'):
+            index = slide_number.replace('-','').replace('/','')
+            index_lower = index.lower()
+            for paths, dirs, files in scandir.walk(folder_path):
+            #for (paths, dirs, files) in os.walk(folder):
+                for file in files:
+                    if index in file or index_lower in file:
+                        # with open(os.path.join(paths, file), 'r') as f:
+                        img_from_docx(os.path.join(paths, file))
         
