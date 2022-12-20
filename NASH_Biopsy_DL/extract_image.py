fd50842b4416bba6267e50cf682d34f421c425bd
yuktimakhija <bb1190067@iitd.ac.in>
2022-12-20 15:38:34 +0530

    added skipping conditions

--- NASH_Biopsy_DL/extract_image.py
+++ NASH_Biopsy_DL/extract_image.py
@@ -113,4 +113,4 @@ def main(folder_path,excel_path,gt_csv_path):
                             if open(filename)[:5] == '{\\rtf':
                                 get_imgs_from_document(filename, file, 'rtf',gt_df)
 
-main("D:/HISTO and CYTO REPORT/2022/",'copy_nash.xlsx')
+main("D:/HISTO and CYTO REPORT/2022/",'copy_nash.xlsx','ground_truth.csv')
